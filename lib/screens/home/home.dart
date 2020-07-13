d956fcb5bd2cf11ef493be2a9f62bc91838cd4e3
Kvackstudio <kvackstudio@outlook.com>
2020-07-13 10:01:45 +0200

    Remodelled body

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -6,6 +6,7 @@ import 'package:provider/provider.dart';
 
 //Måste alltid vara med
 void main() => runApp(Home());
+final title = 'Angry Dentist Home Page';
 
 // Inloggad user, vid instansiering av classen kommer detta att vara null
 var currentUser;
@@ -13,19 +14,43 @@ var currentUser;
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
     //Om det finns en inloggad user kommer variabeln att få informationen
     currentUser = Provider.of<User>(context);
     // marcus tandtroll ID: m4n1afnoP1hK2ST1d5KCfC6xAez2
+    final AuthService _auth = AuthService();
 
     return MaterialApp(
       theme:
           ThemeData.dark().copyWith(scaffoldBackgroundColor: Colors.blueAccent),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Buttons(), // <-- skapar din widget
+        appBar: AppBar(
+          title: Text(title),
+          actions: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.person),
+              label: Text('Logout'),
+              onPressed: () async {
+                await _auth.signOut();
+              },
+            ),
+          ],
         ),
+        body: new Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              new Text(
+                "Hello (username)",
+                style: new TextStyle(
+                    fontSize: 20.0,
+                    color: const Color(0xFF000000),
+                    fontWeight: FontWeight.w200,
+                    fontFamily: "Roboto"),
+              ),
+              Buttons()
+            ]),
       ),
     );
   }
@@ -37,65 +62,57 @@ class Buttons extends StatefulWidget {
 }
 
 class _ButtonsState extends State<Buttons> {
-  final AuthService _auth = AuthService();
-
   @override
   Widget build(BuildContext context) {
-    var title = 'Angry Dentist Home Page';
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Logout'),
-            onPressed: () async {
-              await _auth.signOut();
+    return Container(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          RaisedButton(
+            onPressed: () {
+              print('Tryckte på flour');
+              //Anropar metod i classen Activities för att spara information (behövs inte en egen class för det, men koden blir snyggare så)
+              //Du får fylla i för resten av aktivitererna
+              new Activities().saveActivity("Flour", currentUser.uid);
             },
+            child: Text(
+              'Flour',
+              style: new TextStyle(fontSize: 12.0),
+            ),
+            color: Colors.redAccent,
+            textColor: Colors.white,
+            padding: EdgeInsets.all(10.0),
+            splashColor: Colors.yellowAccent,
+          ),
+          RaisedButton(
+            onPressed: () {
+              print('Tryckte på tandtråd');
+            },
+            child: Text(
+              'Tandtråd',
+              style: new TextStyle(fontSize: 12.0),
+            ),
+            color: Colors.redAccent,
+            textColor: Colors.white,
+            padding: EdgeInsets.all(10.0),
+            splashColor: Colors.yellowAccent,
+          ),
+          RaisedButton(
+            onPressed: () {
+              print('Tryckte på borsta tänderna');
+            },
+            child: Text(
+              'Borsta tänderna',
+              style: new TextStyle(fontSize: 12.0),
+            ),
+            color: Colors.red,
+            textColor: Colors.white,
+            padding: EdgeInsets.all(10.0),
+            splashColor: Colors.yellowAccent,
           ),
         ],
-      ),
-      body: Container(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            RaisedButton(
-              onPressed: () {
-                print('Tryckte på flour');
-                //Anropar metod i classen Activities för att spara information (behövs inte en egen class för det, men koden blir snyggare så)
-                //Du får fylla i för resten av aktivitererna
-                new Activities().saveActivity("Flour", currentUser.uid);
-              },
-              child: Text('Flour'),
-              color: Colors.redAccent,
-              textColor: Colors.white,
-              padding: EdgeInsets.all(10.0),
-              splashColor: Colors.yellowAccent,
-            ),
-            RaisedButton(
-              onPressed: () {
-                print('Tryckte på tandtråd');
-              },
-              child: Text('Tandtråd'),
-              color: Colors.redAccent,
-              textColor: Colors.white,
-              padding: EdgeInsets.all(10.0),
-              splashColor: Colors.yellowAccent,
-            ),
-            RaisedButton(
-              onPressed: () {
-                print('Tryckte på borsta tänderna');
-              },
-              child: Text('Borsta tänderna'),
-              color: Colors.red,
-              textColor: Colors.white,
-              padding: EdgeInsets.all(10.0),
-              splashColor: Colors.yellowAccent,
-            ),
-          ],
-        ),
       ),
     );
   }
