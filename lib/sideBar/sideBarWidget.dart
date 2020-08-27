4097c9bac6fd74ac6450f36b216100e2733da493
Phatchari <phatchari.jansson@microdeb.se>
2020-08-27 15:59:27 +0200

    change stateless to stateful widget

--- lib/sideBar/sideBarWidget.dart
+++ lib/sideBar/sideBarWidget.dart
@@ -40,7 +40,7 @@ class SideBarWidget extends StatelessWidget {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => GroupedBarChart.getData(DateTime.now()),
+                  builder: (context) => ChartsWidget(),
                 ),
               ),
             },
--- lib/widgets/chartsWidget.dart
+++ lib/widgets/chartsWidget.dart
@@ -1,4 +1,3 @@
-
 import 'package:AngryDentist/models/activityData.dart';
 import 'package:AngryDentist/screens/home/home.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -8,10 +7,117 @@ import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:intl/intl.dart';
 
+class ChartsWidget extends StatefulWidget {
+  final List<charts.Series> seriesList;
+  final dateTime;
+  ChartsWidget({this.dateTime, this.seriesList});
+
+  @override
+  _ChartsWidgetState createState() => _ChartsWidgetState();
+}
+
+/*
+factory ChartsWidget.getData(DateTime dateTime) {
+    return new ChartsWidget(
+      _createSampleData(dateTime), dateTime
+    );
+  }
+*/
+
+class _ChartsWidgetState extends State<ChartsWidget> {
+  List<charts.Series> seriesList;
+
+  @override
+  Widget build(BuildContext context) {
+    return new charts.BarChart(
+      seriesList,
+      barGroupingType: charts.BarGroupingType.grouped,
+    );
+  }
+}
+
+List<charts.Series<ActivityData, String>> _createSampleData(DateTime dateTime) {
+ // var stats = List<charts.Series<ActivityData, String>>();
+
+  DateFormat dateYearMonth = DateFormat("yyyyMM");
+
+  int teethBrushedMorning = 0;
+  int teethBrushedNight = 0;
+
+  Firestore.instance
+      .collection("activities")
+      .document(currentUser.userId)
+      .collection(dateYearMonth.format(dateTime))
+      .getDocuments()
+      .then((value) {
+    //Keep track of month fetched to avoid multiple reads of same month
+    value.documents.forEach((n) {
+      print(teethBrushedNight);
+      print("test");
+      print(teethBrushedMorning);
+
+      if (n.data != null) {
+        if (n.documentID.endsWith("M")) {
+          if (n.data["teethBrushed"] == true) {
+            teethBrushedMorning++;
+          }
+        } else if (n.documentID.endsWith("N")) {
+          if (n.data["teethBrushed"] == true) {
+            teethBrushedNight++;
+          }
+        }
+      }
+
+      final teethBrushedData = [
+        new ActivityData('Morning', teethBrushedMorning),
+        new ActivityData('Night', teethBrushedNight),
+      ];
+      final fluorineData = [
+        new ActivityData('Morning', 5),
+        new ActivityData('Night', 25),
+      ];
+      final flossData = [
+        new ActivityData('Morning', 5),
+        new ActivityData('Night', 25),
+      ];
+
+      return [
+        new charts.Series<ActivityData, String>(
+          id: 'teethBrushed',
+          domainFn: (ActivityData activity, _) => activity.yearMonth,
+          measureFn: (ActivityData activity, _) => activity.amount,
+          data: teethBrushedData,
+          colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+          fillColorFn: (_, __) =>
+              charts.MaterialPalette.blue.shadeDefault.lighter,
+        ),
+        new charts.Series<ActivityData, String>(
+          id: 'fluorine',
+          domainFn: (ActivityData activity, _) => activity.yearMonth,
+          measureFn: (ActivityData activity, _) => activity.amount,
+          data: fluorineData,
+          colorFn: (_, __) => charts.MaterialPalette.green.shadeDefault,
+          // fillColorFn: (_, __) => charts.MaterialPalette.transparent,
+        ),
+        new charts.Series<ActivityData, String>(
+          id: 'floss',
+          domainFn: (ActivityData activity, _) => activity.yearMonth,
+          measureFn: (ActivityData activity, _) => activity.amount,
+          data: flossData,
+          colorFn: (_, __) => charts.MaterialPalette.purple.shadeDefault,
+          // fillColorFn: (_, __) => charts.MaterialPalette.transparent,
+        ),
+      ];
+    });
+  });
+}
+
+/*
 class GroupedBarChart extends StatelessWidget {
   final List<charts.Series> seriesList;
   final dateTime;
 
+  @override
   GroupedBarChart(this.seriesList, this.dateTime);
 
   factory GroupedBarChart.getData(DateTime dateTime) {
@@ -65,8 +171,8 @@ class GroupedBarChart extends StatelessWidget {
         }
 
         final teethBrushedData = [
-          new ActivityData('Morning', 5),
-          new ActivityData('Night', 25),
+          new ActivityData('Morning', teethBrushedMorning),
+          new ActivityData('Night', teethBrushedNight),
         ];
         final fluorineData = [
           new ActivityData('Morning', 5),
@@ -108,7 +214,10 @@ class GroupedBarChart extends StatelessWidget {
     });
   }
 }
+*/
+
 /*
+Exempel kod
 /// Bar chart example
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
